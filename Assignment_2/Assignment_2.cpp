7cb443248e9e599162f23b1c887ecf98f33683fb
Mohamed <45342903+TheOneQuantum@users.noreply.github.com>
2020-02-02 21:29:36 -0500

    Finished.

--- Assignment_2/Assignment_2.cpp
+++ Assignment_2/Assignment_2.cpp
@@ -14,7 +14,7 @@ Total Points: 20
 Assignment #: amazon_orders.cpp
 
 Description:
-
+this program is created to gather data from current record (order record) and process theose records and formate them in the desired formate
 *************************************************************************************************************************/
 
 //Include the following
@@ -65,38 +65,38 @@ void process(order_record &);
 //*************************************************************************************
 
 void input(ifstream & infile, order_record & customer_record) // example using the call by reference mechanism in C++ -- call record is passed by reference --note & operator
-{
+{// function is getting the data through file input and store those values into the class variables through the use of the object customer_record
 	infile >> customer_record.cell_number;
 	infile >> customer_record.item_number;
 	infile >> customer_record.quantity;
 	infile >> customer_record.price;
 	infile >> customer_record.processing_plant;
 
-	//add more code to read the rest of the fields (item_number, quantity, price and processing_plant) into the 
-	//order record, p_record.
+	//add more code to read the rest of the fields (item_number, quantity, price and processing_plant) into the
+	// customer_record
+	
 }
 
-///*************************************************************************************
-//Name:  output
-//Precondition: State what is true before the function is called.
-//Postcondition: State what is true after the function has executed.
-//Description:  Describe what the function does (purpose).
-//*************************************************************************************
+
 
 void output(const order_record & customer_record)
 {
 	//Use thee following statement to help you format you our output. These statements are called the magic formula.
-	cout.setf(ios::showpoint);
-	cout.precision(2);
-	cout.setf(ios::fixed);
+
 	/********************************************/
-
-
+	cout.setf(ios::showpoint);
+	cout.setf(ios::fixed);
+	cout.precision(2);
+	// print the value of the object properties input from the input file
 	cout << customer_record.cell_number << "\t";
 	cout << customer_record.item_number << "\t";
 	cout << customer_record.quantity << "\t";
 	cout << customer_record.price << "\t";
 	cout << customer_record.processing_plant << "\t";
+	cout << customer_record.tax_rate << "\t";
+	cout << customer_record.order_tax << "\t";
+	cout << customer_record.net_cost << "\t";
+	cout << customer_record.total_cost << "\t\n";
 	//add more code to print all the fields in the order record
 	
 
@@ -110,18 +110,52 @@ void output(const order_record & customer_record)
 //*************************************************************************************
 
 //Note: there is one 1 input/output parameter
-void process(order_record & customer_record)
+void process(order_record& customer_record)
 {
-	double procesPLant = customer_record.processing_plant ;
-	if (0 <= customer_record.processing_plant)
-	{
-		if (customer_record.processing_plant)
-		{
-
-		}
-	}
-	//put your code here to process/calculate the tax_rate, order_tax, net_cost, and total_cost
+	//Here I created local variables to the function process,
+	//to that the calculation is easy to read.
 	
+	
+	double taxRate;
+	double orderTax;
+	double netCost;
+	double totalCost;
+	double quantity = customer_record.quantity;
+	double price = customer_record.price;
+	int processPlant = customer_record.processing_plant;
+
+	
+
+	if (processPlant >= 0 && processPlant <= 50)
+	{
+		taxRate = 6.0;
+	}
+	else if (processPlant >= 51 && processPlant <= 110)
+	{
+		taxRate = 7.0;
+	}
+	else if (processPlant >= 111 && processPlant <= 200)
+	{
+		taxRate = 8.0;
+	}
+	else if (processPlant >= 201 && processPlant <= 500)
+	{
+		taxRate = 9.0;
+	}
+	else if (processPlant > 500)
+	{
+		taxRate = 11.0;
+	}
+
+	customer_record.tax_rate = taxRate;
+	orderTax = quantity * price * (taxRate / 100);
+	customer_record.order_tax = orderTax;
+	netCost = quantity * price;
+	customer_record.net_cost = netCost;
+	totalCost = netCost + orderTax;
+	customer_record.total_cost = totalCost;
+
+
 
 }
 
@@ -136,8 +170,7 @@ int main()
 	
 	if (infile.fail())
 	{
-		cerr << "Input file did not open correctly" << endl;
-		exit(1);
+		cout << "Input file did not open correctly" << endl;
 
 	}
 	else
@@ -145,9 +178,11 @@ int main()
 		while (!infile.eof())
 		{
 			input(infile, customer_record);
-			//process(customer_record);
-			//output(customer_record);
+			process(customer_record);
+			output(customer_record);
 		}
+		
+		
 	}
 
 	infile.close();
